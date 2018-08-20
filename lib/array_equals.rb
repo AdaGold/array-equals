c537e0a1f75269632b99c8a4495dcd5eb338a1d9
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-08-19 23:04:53 -0700

    Added array_equals

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -5,5 +5,22 @@ require "pry"
 require "awesome_print"
 
 def array_equals(array1, array2)
-  raise NotImplementedError
+  if array1 == nil || array2 == nil
+    unless array1 == nil && array2 == nil
+      return false
+    end
+  elsif array1 == [] || array2 == []
+    unless array1 == [] && array2 == []
+      return false
+    end
+  elsif array1.size != array2.size
+    return false
+  elsif
+    array1.each_with_index do |n,i|
+      if n != array2[i]
+        return false
+      end
+    end
+  end
+  return true
 end
